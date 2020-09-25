db04952c0cfacafee1917cbdd079d99fd4a09321
missteran5169 <70241864+missteran5169@users.noreply.github.com>
2020-09-24 22:53:30 -0400

    Assignment 4

---
+++ JupyterNotebooks/Melissa T Lab and Assignments/Melissa_Teran_Assignment_4.py
@@ -0,0 +1,11 @@
+#calling the function
+def variable(a,b):
+    var = a*b
+    return (var)
+variable(1,2)
+
+#function which will sum a range of integers
+for x in range(10):
+    print(x)
+
+print(sum(range(10)))
