c201563e81e86c78bbab99a7d5406d3389e75d83
missteran5169 <70241864+missteran5169@users.noreply.github.com>
2020-09-30 23:39:13 -0400

    Assignment 5

--- Assignments/Assignment 5/wet_code copy.py
+++ Assignments/Assignment 5/wet_code copy.py
@@ -9,9 +9,17 @@ def saturdays_bank_transactions(transations) -> (float, float):
     savings = 1096.25
     checking = 1590.80
 
-
-
-    pass
 if __name__ == "__main__":
-    transations = []
+    transations = [300.00, -50.00, -5.00, -20, 15.72, 2083.93, -1034.00, -420.00, -5.23, -15.93, -72.90]
     new_balance = saturdays_bank_transactions(transations)
+
+a == 0
+b == -1
+if transations[0:] > a:
+    checking += (transations[0] * 0.85)
+    savings += (transations[0] * 0.15) 
+    return checking
+else:
+    return savings
+
+print("Your new checking balance is:", '${:.2f}'.format(round(new_balance[0], 2)), "\n", "Your new savings balance is:", '${:.2f}'.format(round(new_balance[1], 2)))
