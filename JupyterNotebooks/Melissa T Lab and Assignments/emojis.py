de3ee9bf54af8d22916bdc7df4b249ea071615cf
missteran5169 <70241864+missteran5169@users.noreply.github.com>
2020-11-10 20:13:04 -0500

    lab 8

--- JupyterNotebooks/Melissa T Lab and Assignments/emojis.py
+++ JupyterNotebooks/Melissa T Lab and Assignments/emojis.py
@@ -1,1 +1,7 @@
 import emoji
+print(emoji.emojize(":thumbs_up:", use_aliases=True))
+print(emoji.emojize(":angry:", use_aliases=True))
+print(emoji.emojize(":cry:", use_aliases=True))
+print(emoji.demojize('👍'))
+print(emoji.demojize('😠'))
+print(emoji.demojize('😢'))
