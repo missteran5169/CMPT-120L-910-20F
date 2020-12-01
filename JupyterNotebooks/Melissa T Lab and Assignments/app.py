6372faf656c94ef79a23176e6bf6fe7b44a4f16a
missteran5169 <70241864+missteran5169@users.noreply.github.com>
2020-11-30 19:07:41 -0500

    lab 9

--- JupyterNotebooks/Melissa T Lab and Assignments/app.py
+++ JupyterNotebooks/Melissa T Lab and Assignments/app.py
@@ -3,6 +3,10 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def index():
+    app.logger.info("User traveled to the info route")
     return render_template("index.html")
-Hello = Hello
+@app.route('/hello')
+def hello_world():
+    app.logger.warning("This page isn't finished yet but we say hello.")
+    return 'Hello, Me'
